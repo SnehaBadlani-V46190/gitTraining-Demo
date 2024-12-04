7aaa92f952d57aeb448900ce21d6f801b3d38cc7
SnehaBadlani-V46190 <SBadlani@aflac.com>
2024-12-04 15:27:22 +0530

    removed unnecessary line

--- Demo.java
+++ Demo.java
@@ -4,10 +4,7 @@ public class Demo
  {
     public static void main(String[] args) {
       System.out.println("Hello, Sneha!");
-<<<<<<< HEAD
       // comment 2
-=======
       // comment 1
->>>>>>> main
-  }
+ }
 }
