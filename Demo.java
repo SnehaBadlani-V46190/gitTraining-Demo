10aed0011bb6757ff41fef19824ae163b01143bd
SnehaBadlani-V46190 <SBadlani@aflac.com>
2024-12-04 15:42:16 +0530

    Added number 2 to sop

--- Demo.java
+++ Demo.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Demo
  {
     public static void main(String[] args) {
-      System.out.println("Hello, Sneha!");
+      System.out.println("Hello, Sneha!"+2);
       System.out.println("Hello, Badlani!");
       // comment 2
       // comment 1
