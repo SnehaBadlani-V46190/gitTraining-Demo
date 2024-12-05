bac549a3421641ba35e97da01366668d451051c8
SnehaBadlani-V46190 <SBadlani@aflac.com>
2024-12-05 18:17:59 +0530

    changed SOP

--- Demo.java
+++ Demo.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class Demo
  {
     public static void main(String[] args) {
-      System.out.println("Hello, Sneha!"+1);
       System.out.println("Hello, World!");
        }
 }
