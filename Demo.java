8e3a94f342c09c5821dded5f718db0acca5f96da
SnehaBadlani-V46190 <SBadlani@aflac.com>
2024-12-05 17:20:59 +0530

    Deleted comments and last sop

--- Demo.java
+++ Demo.java
@@ -5,8 +5,5 @@ public class Demo
     public static void main(String[] args) {
       System.out.println("Hello, Sneha!"+1+"How are you?");
       System.out.println("Hello, Badlani!");
-      // comment 2
-      // comment 1
-      System.out.println("Hello, Sneha!"+1+"Welcome home"); 
        }
 }
