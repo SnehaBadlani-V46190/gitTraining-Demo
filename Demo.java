dd10d115e9bd39bb5b3860ab478aa47397d2753e
SnehaBadlani-V46190 <SBadlani@aflac.com>
2024-12-06 11:37:24 +0530

    Changed SOP and added a number

--- Demo.java
+++ Demo.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Demo
  {
     public static void main(String[] args) {
-      System.out.println("Hello, India!");
+      System.out.println("Hello, India!"+2);
       // comment1
        }
 }
